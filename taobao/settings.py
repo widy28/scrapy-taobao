852a591302c30a29612dd52f3cb6262acb01257b
widy28 <widy28@163.com>
2015-05-15 14:28:51 +0800

    Create settings.py

---
+++ taobao/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for taobao project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'taobao'
+
+SPIDER_MODULES = ['taobao.spiders']
+NEWSPIDER_MODULE = 'taobao.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'taobao (+http://www.yourdomain.com)'
